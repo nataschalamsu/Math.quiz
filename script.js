1ba938114472ad013e35fe80db451ee83a906f2c
acha <tazcha15@gmail.com>
2018-03-03 14:11:23 +0700

    third modified on js and second modified on html

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ var operator = ['+', '*', '-', '/'];
 var num1 = Math.round(Math.random() * 100 + 1);
 var num2 = Math.round(Math.random() * 100 + 1);
 var index = Math.floor(Math.random() * operator.length);
-var life = 3;
-var score = 0;
 var questions = document.getElementById('questions');
-var currentLife = document.getElementById('currentLife');
-var currentScore = document.getElementById('score');
 var userStatus = document.getElementById('status');
 var userAnswers = document.getElementById('answers');
 var submit = document.getElementById('submit');
@@ -16,15 +12,13 @@ window.onload = function() {
   quizzes();
 }
 
-answerCalculate();
-
 function answerCalculate() {
-  switch(index) {
-    case 0: answers = num1 + num2; break;
-    case 1: answers = num1 * num2; break;
-    case 2: answers = num1 - num2; break;
-    case 3: answers = num1 / num2; break;
-  }
+  switch(index) {
+    case 0: answers = num1 + num2; break;
+    case 1: answers = num1 * num2; break;
+    case 2: answers = num1 - num2; break;
+    case 3: answers = num1 / num2; break;
+  }
 }
 
 function quizzes() {
@@ -33,12 +27,11 @@ function quizzes() {
   questions.innerHTML = num1 + ' ' + operator[index] + ' ' + num2;
 }
 
-
-
 submit.onclick = function() {checkAnswer()}
 
 function checkAnswer() {
-  if(userAnswers.value === answerCalculate()) {
+  answerCalculate();
+  if(userAnswers.value == answers) {
     win();
   } else {
     lose();
